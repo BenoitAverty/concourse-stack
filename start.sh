030b773026b12ce14a28c351fdb8dc9457c6f8ca
Benoit Averty <benoit.averty@gmail.com>
2017-06-13 22:51:23 +0200

    crap workaround with wompose and without traefik...

--- start.sh
+++ start.sh
@@ -8,4 +8,5 @@ ssh-keygen -t rsa -f ./keys/worker/worker_key -N ''
 cp ./keys/worker/worker_key.pub ./keys/web/authorized_worker_keys
 cp ./keys/web/tsa_host_key.pub ./keys/worker
 
-CONCOURSE_EXTERNAL_URL=plouf docker stack deploy -c concourse-stack.yml concourse
+docker network create --attachable --driver overlay concourse-network
+CONCOURSE_EXTERNAL_URL=concourse.docker.ab0.fr docker stack deploy -c concourse-stack.yml concourse
